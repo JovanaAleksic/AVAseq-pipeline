adddcd5726dfd42b617d7fcc34d7e37098cd5851
Jovana Aleksic <53390106+JovanaAleksic@users.noreply.github.com>
2022-09-16 13:30:17 +0300

    pairwise aligner

--- fragment_matcher.py
+++ fragment_matcher.py
@@ -1,9 +1,10 @@
 from parameters import *
+from Bio.Align import PairwiseAligner
 
 
 
 # def fragment_alignment(fragment_sequences)
-
+aligner = PairwiseAligner(match_score=1.0)
 fragSeq = {}
 
 # create dictionary
@@ -14,12 +15,32 @@ for line in open(fragment_sequences, "r").readlines():
 	# replace _ with :
 	key = key.split("_")
 	key = key[0] + "_" + key[1] + ":" + key[2]
-	seq = line[1]
+	seq = line[1][0:75]  # important to not provide the whole sequence
 
-	fragSeq[key] = seq
+	fragSeq[seq] = key
 
-print(fragSeq)
-		# return 
+list_of_sequences = fragSeq.keys()
+
+
+# test it
+for seqq in list_of_sequences:
+	query = "ABCF" + seqq[4:50] + "GXHJS" + seqq[55:75]
+	real_fragment = fragSeq[seqq]
+
+	score = 0
+	matched_fragment=''
+
+	for seq in list_of_sequences:
+		# print(fragSeq[seq])
+		temp_score = aligner.score(query, seq)
+		# print(temp_score)
+		if temp_score >= score:
+			score = temp_score
+			matched_fragment = fragSeq[seq]
+
+	print(real_fragment)
+	print(matched_fragment)
+	print(score)
 
 
 
